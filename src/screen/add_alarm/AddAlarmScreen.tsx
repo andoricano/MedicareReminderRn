fb2906e35a9a763aba01a20939f54e25a852cca3
andoricano <andoricano@gmail.com>
2025-09-12 00:17:19 +0900

    [feat] add HeadComponent and Stepper Component to AddAlarmScreen.

--- src/screen/add_alarm/AddAlarmScreen.tsx
+++ src/screen/add_alarm/AddAlarmScreen.tsx
@@ -1,49 +1,71 @@
 import React, { useState } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { Pressable, Text, View, Button, StyleSheet } from 'react-native';
+import { ScreenHeader } from '../../components/ScreenHeader';
 
 import BaseScreen from '../BaseScreen';
 import { useManager } from '../../ManagerContext';
-
+import { NumberStepper } from '../../components/NumberStepper';
+import text from '../../locales/ko.json'
 
 type AddingAlarm = {
-  daily : number,
-  todoNum : number,
-  alarmList : number[]
+  cycle: number,
+  todoNum: number,
+  alarmList: number[]
 };
 
 export default function AddAlarmScreen({ navigation, route }: any) {
   const [alarmValues, setAlarmValues] = useState<AddingAlarm>({
-    daily: -1,
+    cycle: 0,
     todoNum: -1,
-    alarmList : [],
+    alarmList: [],
   });
   const { potion } = route.params; console.log("potion:" + JSON.stringify(potion));
   const { addPotionCtx } = useManager();
   return (
-    <BaseScreen style={{ flex: 1 }}>
-      <Button
-        title="back"
-        onPress={() => navigation.pop()}
-      />
-      <View style={{ width: 250, margin: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Button title="allcheck" onPress={() => { }} />
-        <Button title="2day check" onPress={() => { }} />
+    <BaseScreen style={{ flex: 1, justifyContent: 'space-between' }}>
 
-      </View>
-        <Button
-          title="Done"
-          onPress={async () => {
-            await addPotionCtx(potion);
-            navigation.pop(2);
+      <View>
+        <ScreenHeader
+          navigation={navigation}
+          title={text.add_alarm_title}
+        />
+
+        <NumberStepper
+          Label={() =>
+            <Text style={styles.scheme}>
+              {text.add_alarm_cycle_label}
+            </Text>
+          }
+          min = {0}
+          max = {24}
+          onClick={(num) =>
+            setAlarmValues(prev => ({ ...prev, cycle: num }))
+          }
+          stepperStyle={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 10,
+            padding: 10,
+            borderRadius: 8,
           }}
         />
+
+      </View>
+
+      <Button
+        title="Done"
+        onPress={async () => {
+          await addPotionCtx(potion);
+          navigation.pop(2);
+        }}
+      />
     </BaseScreen>
   );
 }
 
 const styles = StyleSheet.create({
-  title: { fontSize: 30, fontWeight: 'bold', marginBottom: 20 },
-  scheme: { fontSize: 23, fontWeight: 'bold', marginBottom: 8 },
+  scheme: { fontSize: 23, fontWeight: 'bold', marginTop: 12, marginBottom: 8 },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
